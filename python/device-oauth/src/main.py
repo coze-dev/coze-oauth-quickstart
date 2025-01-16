3986304b6b6911dcdd14a5e8cb5bac3d5835a9ab
chyroc <chyroc@qq.com>
2025-01-16 10:24:07 +0800

    python-pkce-quickstart

--- python/device-oauth/src/main.py
+++ python/device-oauth/src/main.py
@@ -1,16 +1,20 @@
-from cozepy import DeviceOAuthApp, load_oauth_app_from_config, JWTOAuthApp
-from datetime import datetime, timedelta
+from datetime import datetime
+
+from cozepy import DeviceOAuthApp, load_oauth_app_from_config
 
 COZE_OAUTH_CONFIG_PATH = "coze_oauth_config.json"
 
-def load_coze_oauth_app(config_path)->DeviceOAuthApp:
+
+def load_coze_oauth_app(config_path) -> DeviceOAuthApp:
     with open(config_path, "r") as file:
         config = file.read()
 
     return load_oauth_app_from_config(config)
 
-def timestamp_to_datetime(timestamp: int)->str:
+
+def timestamp_to_datetime(timestamp: int) -> str:
     return datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
+
 
 if __name__ == "__main__":
     coze_oauth_app = load_coze_oauth_app(COZE_OAUTH_CONFIG_PATH)
@@ -26,4 +30,3 @@ if __name__ == "__main__":
     print(f"[device-oauth] refresh_token: {oauth_token.refresh_token}")
     expires_str = timestamp_to_datetime(oauth_token.expires_in)
     print(f"[device-oauth] expires_in: {oauth_token.expires_in} ({expires_str})")
-    
--- python/jwt-oauth/src/main.py
+++ python/jwt-oauth/src/main.py
@@ -1,16 +1,20 @@
+from datetime import datetime
+
 from cozepy import load_oauth_app_from_config, JWTOAuthApp
-from datetime import datetime, timedelta
 
 COZE_OAUTH_CONFIG_PATH = "coze_oauth_config.json"
 
-def load_coze_oauth_app(config_path)->JWTOAuthApp:
+
+def load_coze_oauth_app(config_path) -> JWTOAuthApp:
     with open(config_path, "r") as file:
         config = file.read()
 
     return load_oauth_app_from_config(config)
 
-def timestamp_to_datetime(timestamp: int)->str:
+
+def timestamp_to_datetime(timestamp: int) -> str:
     return datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
+
 
 if __name__ == "__main__":
     coze_oauth_app = load_coze_oauth_app(COZE_OAUTH_CONFIG_PATH)
@@ -20,4 +24,3 @@ if __name__ == "__main__":
     print(f"[jwt-oauth] access_token: {oauth_token.access_token}")
     expires_str = timestamp_to_datetime(oauth_token.expires_in)
     print(f"[jwt-oauth] expires_in: {oauth_token.expires_in} ({expires_str})")
-    
